efa9ed72d85cb75cbae51aeeae9ca14514e609a1
kanathips <s7070013@kmitl.ac.th>
2014-11-16 15:42:47 +0700

    GUI Code
    1. Add Toolbar for control
    2. Add option to exit program and save graph picture

--- GUI.py
+++ GUI.py
@@ -7,7 +7,7 @@ import tkColorChooser
 #import arange from numpy to create x values (float)
 from numpy import arange
 
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 
 #import Figure from matplotlib to be space for ploter 
 from matplotlib.figure import Figure
@@ -29,14 +29,14 @@ class Ploter(object):
     def ploter_frame(self):
    
         frame = Tk.Frame(self.main_win, width=500, height=500) #frame for ploter
-        figure = Figure(figsize=(3,3), dpi=100)# set height and weight of Figure
+        self.figure = Figure(figsize=(3,3), dpi=100)# set height and weight of Figure
 
-        ploter = figure.add_subplot(111) 
+        ploter = self.figure.add_subplot(111) 
         ploter.grid(self.grid_status) #Part for enable or disable grid of ploter
 
-        canvas = FigureCanvasTkAgg(figure, master=frame)
-        canvas.show() #
-        canvas.get_tk_widget().pack(fill=Tk.BOTH, expand=1) #pack canvas to frame
+        self.canvas = FigureCanvasTkAgg(self.figure, master=frame)
+        self.canvas.show() #
+        self.canvas.get_tk_widget().pack(fill=Tk.BOTH, expand=1) #pack canvas to frame
 
         frame.pack(side = Tk.LEFT) #pack frame to root window
 
@@ -47,15 +47,18 @@ class Main_win(object): #GUI class
         self.main_win = Tk.Tk()
         self.main_win.wm_title('Math Ploter') #Set titel of main window
 
+
+
 #Equation box section
 class Equation(object):
 
-    def __init__(self, main_win):
+    def __init__(self, main_win, ploter):
+        self.fig = ploter.figure
         self.line_color = '#000000'
         self.main_win = main_win.main_win
         self.equation_frame()
         
-        
+    #color dialog for graph 
     def colorChooser(self): #function to choose color of graph
         (rgb, hx) = tkColorChooser.askcolor()
         self.color_change.config(fg=hx)
@@ -77,18 +80,23 @@ class Equation(object):
 
 #Menu bar
 class Menu(object):
-    def __init__(self, main_win):
+    def __init__(self, main_win, ploter):
         self.main_win = main_win.main_win
+        self.fig = ploter.figure
+        
         self.menu_bar()
-        
-        
+
+    def main_exit(self):
+        self.main_win.quit()
+        self.main_win.destroy()
+    
     def menu_bar(self):
         menubar = Tk.Menu(self.main_win)
         # create a pulldown menu, and add it to the menu bar
         filemenu = Tk.Menu(menubar, tearoff=0)
-        filemenu.add_command(label="Save")
+        filemenu.add_command(label="Save", command = self.fig.savefig('save'))
         filemenu.add_separator()
-        filemenu.add_command(label="Exit")
+        filemenu.add_command(label="Exit", command = self.main_exit)
         menubar.add_cascade(label="File", menu=filemenu)
 
         # create more pulldown menus
@@ -102,15 +110,24 @@ class Menu(object):
         # display the menu
         self.main_win.config(menu=menubar)
 
+#tool bar class for control ploter 
+class Tool_bar(object):
+
+    def __init__(self, main_win, ploter):
+        self.main_win = main_win.main_win
+        self.canvas = ploter.canvas
         
+        toolbar = NavigationToolbar2TkAgg(self.canvas, self.main_win)
+        toolbar.update()
        
 
 def main():
     main = Main_win()
-    Menu(main)
-    Ploter(main)
+    ploter = Ploter(main)
+    Menu(main, ploter)
+    Tool_bar(main, ploter)
     for _ in xrange(3):
-        Equation(main)
+        Equation(main, ploter)
     Tk.Button(text='Start Plot').pack()
     
     Tk.mainloop()
